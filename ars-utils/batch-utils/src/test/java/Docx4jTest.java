6f9e18f539be88d5612f1d886bc66eed8eb10984
Arsenii Chistii <arsenii@baltinfocom.ru>
2019-05-08 14:23:38 +0300

    Add files

--- ars-utils/batch-utils/src/test/java/Docx4jTest.java
+++ ars-utils/batch-utils/src/test/java/Docx4jTest.java
@@ -7,6 +7,12 @@ public class Docx4jTest {
     @Test
     public void convertFileToXml() {
         Docx4jService docx4jService = new Docx4jService();
-        docx4jService.convertFile("C:\\Users\\User\\Desktop\\tests", "Таблички .docx");
+        docx4jService.convertFile("C:\\Users\\User\\Desktop\\tests", "Цвет Строк (ООО Тополек) Либре .docx");
+    }
+
+    @Test
+    public void testCount() {
+        Integer columnWidth = 1000 / 3;
+        System.out.println(columnWidth);
     }
 }
