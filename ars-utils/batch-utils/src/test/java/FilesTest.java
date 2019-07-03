2c0977fe828b6a0537a34b266d29bc2b648e650e
Arsenii Chistii <arsenii@baltinfocom.ru>
2019-07-03 19:19:58 +0300

    Add files text searching test

--- ars-utils/batch-utils/src/test/java/FilesTest.java
+++ ars-utils/batch-utils/src/test/java/FilesTest.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 import files.TikaConverter;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import files.FilesUtilsHelper;
@@ -30,6 +32,22 @@ public class FilesTest {
         // System.out.println(date.getTime());
         // String dateS = new SimpleDateFormat("YYYY-MM-DD").format(date);
         // System.out.println();
+    }
+
+    @Test
+    public void searchFilesWithText() {
+        String regex = "Работа, подтвержденная свидетельскими показаниям";
+        List<File> files = FilesUtilsHelper.listFilesForFolder(new File("C:\\Users\\User\\Desktop\\tests"));
+        for (File file : files) {
+            try {
+                String txt = FileUtils.readFileToString(file);
+                if (txt.split(regex).length > 1 || txt.toUpperCase().contains(regex.toUpperCase())) {
+                    System.out.println("Matched file: " + file.getAbsolutePath());
+                }
+            } catch (Exception e) {
+                System.out.println("Exception on file:" + file.getAbsolutePath() + "\n\n" + e + "\n   -----------     ");
+            }
+        }
     }
 
     @Test
